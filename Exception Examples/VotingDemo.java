c540ba5182c8a5869e27da9f6d98b012043c2fd7
LOKESH DODLA <87563479+lokeshdodla@users.noreply.github.com>
2021-07-17 20:50:53 +0530

    Exception Examples

---
+++ Exception Examples/VotingDemo.java
@@ -0,0 +1,36 @@
+
+import java.util.*;
+class AgeException extends Exception
+{
+	public AgeException(String s)
+	{
+		System.out.println(s);
+	}
+}
+class VotingDemo
+{
+	public static void main(String[]args)
+	{
+		int age;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter age :");
+		age=sc.nextInt();
+		try
+		{
+			if(age<18)
+			{
+				throw new AgeException("NOT ELEGIBLE FOR VOTING ,AGE MUST BE GREATER THAN 18.");
+			}
+			else
+			{
+				System.out.println("WELCOME TO VOTING");
+			}
+		}
+		catch(Exception e)
+		{
+		}
+	}
+}
+
+
+
